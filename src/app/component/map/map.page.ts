63860995587d686d6064240cfe55308f289ef562
Fourkane-SA <83338736+Fourkane-SA@users.noreply.github.com>
2022-11-23 20:22:08 +0100

    update map

--- src/app/component/map/map.page.ts
+++ src/app/component/map/map.page.ts
@@ -1,18 +1,33 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
 import '@geoman-io/leaflet-geoman-free';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
+
+
+
 export class MapPage implements AfterViewInit {
+  goToHome() {
+    this.router.navigateByUrl('/tab/accueil')
+  }
+  
   private map : L.Map;
 
   posX : number = 45.7663;
   posY : number = 4.8883;
-
+  isOpen: boolean = false
+  @ViewChild('popover') popover;
+  
+  openPopOver(e: Event) {
+    this.popover.event = e;
+    this.isOpen = true;
+  }
 
   setPosition(position) {
     this.posX = position.coords.latitude
@@ -29,7 +44,8 @@ export class MapPage implements AfterViewInit {
     }
     this.map = L.map('map', {
       center: [ this.posX, this.posY ],
-      zoom: 13
+      zoom: 13,
+      zoomControl: false
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -37,18 +53,10 @@ export class MapPage implements AfterViewInit {
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
-
     tiles.addTo(this.map);
-
-    let MyLeafletControl = L.Control.extend({
-      options: {
-        position: 'bottomright'
-      }
-    })
-    
   }
-
-  constructor() { 
+  
+  constructor(public router: Router,) { 
     this.posX = 45.7663
     this.posY = 4.8883
   }
