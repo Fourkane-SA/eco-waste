53b9fc331549ec60628aee21a15ead12a280e712
Mourad Sidhoumi <61125130+mouradsidhoumi@users.noreply.github.com>
2022-12-07 11:37:55 +0100

    recette search + faq text

--- src/app/pages/recette/recette.page.ts
+++ src/app/pages/recette/recette.page.ts
@@ -6,14 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recette.page.scss'],
 })
 export class RecettePage implements OnInit {
-  lifeHCards = [
+  allRecettes = [
     {title: "Guacamole", img: "https://www.papillesetpupilles.fr/wp-content/uploads/2020/08/Guacamole-%C2%A9cookingalamel-Licence-CC-BY-NC-ND-2.0.jpg"},
     {title: "Gateau aux pommes", img: "https://www.mesrecettes.info/wp-content/uploads/2020/04/gateau-aux-pommes-ultra-moelleux-de-grand-mere.jpg"},
     {title: "Exemple", img: "https://via.placeholder.com/200x150"},
   ]
+  recettes = []
+
+  search
+
   constructor() { }
 
   ngOnInit() {
+    
   }
-
+  update() {
+    this.recettes = this.allRecettes.filter(recette => recette.title.toLowerCase().includes(this.search.toLowerCase()))
+  }
 }
